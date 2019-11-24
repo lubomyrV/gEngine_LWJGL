fcc022def81ab9b66a829ca8ae66467c72a5408c
lubomyrV <lubomyr.voloschak@gmail.com>
2019-11-24 20:09:53 +0200

    8_projection matrices

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -1,7 +1,10 @@
 package engineTester;
 
 import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
 
+import entities.Camera;
+import entities.Entity;
 import models.RawModel;
 import models.TextureModel;
 import renderEngine.DisplayManager;
@@ -17,37 +20,99 @@ public class MainGameLoop {
 		DisplayManager.createDisplay();
 		
 		Loader loader = new Loader();
-		Renderer rendere = new Renderer();
 		StaticShader shader = new StaticShader();
+		Renderer rendere = new Renderer(shader);
 		
 		float[] vertices = {            
-                -0.5f,0.5f,0,   //V0
-                -0.5f,-0.5f,0,  //V1
-                0.5f,-0.5f,0,   //V2
-                0.5f,0.5f,0     //V3
+                -0.5f,0.5f,0,   
+                -0.5f,-0.5f,0,  
+                0.5f,-0.5f,0,   
+                0.5f,0.5f,0,        
+                 
+                -0.5f,0.5f,1,   
+                -0.5f,-0.5f,1,  
+                0.5f,-0.5f,1,   
+                0.5f,0.5f,1,
+                 
+                0.5f,0.5f,0,    
+                0.5f,-0.5f,0,   
+                0.5f,-0.5f,1,   
+                0.5f,0.5f,1,
+                 
+                -0.5f,0.5f,0,   
+                -0.5f,-0.5f,0,  
+                -0.5f,-0.5f,1,  
+                -0.5f,0.5f,1,
+                 
+                -0.5f,0.5f,1,
+                -0.5f,0.5f,0,
+                0.5f,0.5f,0,
+                0.5f,0.5f,1,
+                 
+                -0.5f,-0.5f,1,
+                -0.5f,-0.5f,0,
+                0.5f,-0.5f,0,
+                0.5f,-0.5f,1
+        };
+         
+        float[] textureCoords = {
+                0,0,
+                0,1,
+                1,1,
+                1,0,            
+                0,0,
+                0,1,
+                1,1,
+                1,0,            
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0
         };
          
         int[] indices = {
-                0,1,3,  //Top left triangle (V0,V1,V3)
-                3,1,2   //Bottom right triangle (V3,V1,V2)
-        };
-        
-        float[] textureCoords = {
-        	0,0, //V0
-        	0,1, //V1
-        	1,1, //V2
-        	1,0  //V3
+                0,1,3,  
+                3,1,2,  
+                4,5,7,
+                7,5,6,
+                8,9,11,
+                11,9,10,
+                12,13,15,
+                15,13,14,   
+                16,17,19,
+                19,17,18,
+                20,21,23,
+                23,21,22
         };
 		
 		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
-		ModelTexture texture = new ModelTexture(loader.loadTexture("sq"));
-		TextureModel textureModel = new TextureModel(model, texture);
-				
+		
+		TextureModel staticModel = new TextureModel(model, new ModelTexture(loader.loadTexture("sq")));
+		
+		Entity entity = new Entity(staticModel, new Vector3f(0,0,-5),0,0,0,1);
+		
+		Camera camera = new Camera();
+		
 		while(!Display.isCloseRequested()) {
-			//game logic
+			//entity.increasePosition(0, 0, 0);
+			entity.increaseRotation(1, 1, 0);
+			camera.move();
 			rendere.prepare();
 			shader.start();
-			rendere.render(textureModel);
+			shader.loadViewMatrix(camera);
+			rendere.render(entity, shader);
 			shader.stop();
 			DisplayManager.updateDisplay();
 		}
--- src/shaders/StaticShader.java
+++ src/shaders/StaticShader.java
@@ -2,12 +2,17 @@ package shaders;
 
 import org.lwjgl.util.vector.Matrix4f;
 
+import entities.Camera;
+import toolbox.Maths;
+
 public class StaticShader extends ShaderProgram{
 
 	private static final String VERTEX_FILE = "src/shaders/vertexShader.vert";
     private static final String FRAGMENT_FILE = "src/shaders/fragmentShader.frag";
     
     private int locationTransformationMatrix;
+    private int locationProjectionMatrix;
+    private int locationViewMatrix;
 	
 	public StaticShader() {
 		super(VERTEX_FILE, FRAGMENT_FILE);
@@ -22,10 +27,21 @@ public class StaticShader extends ShaderProgram{
 	@Override
 	protected void getAllUniformLocations() {
 		locationTransformationMatrix = super.getUniformLocation("transformationMatrix");
+		locationProjectionMatrix = super.getUniformLocation("projectionMatrix");
+		locationViewMatrix = super.getUniformLocation("viewMatrix");
 	}
 	
 	public void loadTransformationMatrix(Matrix4f matrix) {
 		super.loadMatrix(locationTransformationMatrix, matrix);
+	}
+	
+	public void loadViewMatrix(Camera camera) {
+		Matrix4f viewMatrix = Maths.createViewMatrix(camera);
+		super.loadMatrix(locationViewMatrix, viewMatrix);
+	}
+	
+	public void loadProjectionMatrix(Matrix4f projection) {
+		super.loadMatrix(locationProjectionMatrix, projection);
 	}
 
 }
